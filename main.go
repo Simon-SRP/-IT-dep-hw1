e33996491900ed633732cb6cf0598129b76c14af
Simon-SRP <128226779+Simon-SRP@users.noreply.github.com>
2024-11-10 16:18:51 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,43 +7,70 @@ import (
 	"strings"
 )
 
-func makeUnicString(file_name string) ([]string, error) {
+func UP(uniqueLines map[string]bool) []string {
+	result := make([]string, 0, len(uniqueLines))
+	for line := range uniqueLines {
+		result = append(result, line)
+	}
+	for i := range result {
+		result[i] = strings.ToUpper(result[i])
+	}
+	return result
+}
+
+func findUnicString(file_name string) ([]string, error) {
 
 	file, err := os.Open(file_name)
 	if err != nil {
-		return []string{}, fmt.Errorf("Файл нельзя прочитать %v", err)
+		return []string{}, fmt.Errorf("Ошибка открытия файла %w", err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	un_lines := make(map[string]bool)
+	uniqueLines := make(map[string]bool)
 	for scanner.Scan() {
 		line := scanner.Text()
-		un_lines[line] = true
+		uniqueLines[line] = true
 	}
 	if err := scanner.Err(); err != nil {
 		return nil, fmt.Errorf("ошибка сканирования файла: %w", err)
 	}
 
-	res := make([]string, 0, len(un_lines))
-	for line := range un_lines {
-		res = append(res, line)
+	result := UP(uniqueLines)
+	return result, err
+}
+
+func outputsResult(filename string, result *os.File) error {
+	uniqLines, err := findUnicString(filename)
+	if err != nil {
+		return fmt.Errorf("Ошибка поиска уникальных строк: %w", err)
 	}
-	for i := range res {
-		res[i] = strings.ToUpper(res[i])
+
+	for _, line := range uniqLines {
+		_, err := result.WriteString(fmt.Sprintf("%s - %d байт\n", line, len(line)))
+		if err != nil {
+			return fmt.Errorf("Ошибка записи в файл: %w", err)
+		}
 	}
-	return res, err
+
+	return nil
 }
 
 func main() {
-	res, err := os.Create("result.txt")
-	if err != nil {
-		fmt.Println("ошибка создания файла: %v", err)
+	if len(os.Args) < 2 {
+		fmt.Println("вы не указали путь к файлу")
 		return
 	}
 	name := os.Args[1]
-	uniqLines, err := makeUnicString(name)
-	for i := range uniqLines {
-		res.WriteString(fmt.Sprintf("%s - %d байт\n", uniqLines[i], len(uniqLines[i])))
+	result, err := os.Create("resulted.txt")
+	if err != nil {
+		fmt.Println(fmt.Errorf("ошибка создания файла: %w", err))
+		return
+	}
+	defer result.Close()
+	err = outputsResult(name, result)
+	if err != nil {
+		fmt.Println(fmt.Errorf("ошибка записи в файл: %w", err))
+		return
 	}
 }
